fb0e6f7f94b9e46c3f02bd425289f722c2604274
JandroCode <algarrobeando@hotmail.com>
2024-11-13 16:09:41 +0100

    Creando una página de NOT - FOUND

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,1 +1,29 @@
-<h2>PÁGINA NO ENCONTRADA</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <title></title>
+    <link rel="stylesheet" href="mazer/css/bootstrap.css">
+    <link rel="stylesheet" href="mazer/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="mazer/css/app.css">
+    <link rel="stylesheet" href="mazer/css/pages/auth.css">
+    <link rel="stylesheet" href="css/site.css">
+</head>
+
+<body>
+    <div id="error">
+        <div class="error-page container mt-5">
+            <div class="col-md-8 col-12 offset-md-2">
+                <div class="text-center">
+                    <h1 class="error-title">NO ENCONTRADA</h1>
+                    <p class='fs-5 text-gray-600'>La página que buscas no se encuentra disponible.</p>
+                    <a href="index.html" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
